d44d42b15ff7429d36dea862bf88c58d45f8d1d6
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-13 23:03:49 +0300

    Ders-137

---
+++ breakPoint/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  breakPoint
+//
+//  Created by Ahmed Selim Üzüm on 13.07.2018.
+//  Copyright © 2018 Ahmed Selim Üzüm. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
