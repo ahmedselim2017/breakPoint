3205324231b74b5633d0644f2e77ef76144fe905
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-14 02:42:24 +0300

    Ders-141

---
+++ breakPoint/Araclar/Sabitler.swift
@@ -0,0 +1,13 @@
+//
+//  Sabitler.swift
+//  breakPoint
+//
+//  Created by Ahmed Selim Üzüm on 14.07.2018.
+//  Copyright © 2018 Ahmed Selim Üzüm. All rights reserved.
+//
+
+import Foundation
+
+//StoryBoard Id'leri
+let SB_ID_AuthVC="AuthVC";
+let SB_ID_GirisYapVC="GirisYapVC";
