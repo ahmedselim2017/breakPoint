e8f8f93fd94946a468ff15c3a4a66e73d66c40e7
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-14 03:00:02 +0300

    Ders-142

---
+++ breakPoint/Servisler/AuthServisi.swift
@@ -0,0 +1,41 @@
+//
+//  AuthServisi.swift
+//  breakPoint
+//
+//  Created by Ahmed Selim Üzüm on 14.07.2018.
+//  Copyright © 2018 Ahmed Selim Üzüm. All rights reserved.
+//
+
+import Foundation;
+import Firebase;
+
+class AuthServisi{
+    static let ornek=AuthServisi();
+    
+    func kullaniciKaydet(eposta:String,sifre:String,kullaniciKaydetmeBitis:@escaping(_ durum:Bool, _ hata:Error?)->()){
+        Auth.auth().createUser(withEmail: eposta, password: sifre) { (kullanici, hata) in
+            
+            guard let kullanici = kullanici else{
+                kullaniciKaydetmeBitis(false,hata);
+                return;
+            }
+            
+            let kullaniciVeri=["saglayici":kullanici.user.providerID,"eposta":kullanici.user.email];
+            
+            VeriServisi.ornek.VTKullaniciOlustur(id: kullanici.user.uid, kullaniciVeri: kullaniciVeri);
+            kullaniciKaydetmeBitis(true,nil);
+
+        }
+    }
+    
+    func kullaniciGirisYap(eposta:String,sifre:String,kullaniciGirisBitis:@escaping(_ durum:Bool, _ hata:Error?)->()){
+        Auth.auth().signIn(withEmail: eposta, password: sifre) { (kullanici, hata) in
+            guard let kullanici=kullanici else{
+                kullaniciGirisBitis(false,hata);
+                return;
+            }
+            kullaniciGirisBitis(true,nil);
+        }
+    }
+    
+}
